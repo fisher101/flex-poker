99e2684045a1989e9c176ecd1cdae33e1d9effd9
fisher101 <134185919+fisher101@users.noreply.github.com>
2025-01-26 12:03:58 +0800

    Update sign-up-step-2-form.tsx

--- src/main/webapp/resources/modules/signup/sign-up-step-2-form.tsx
+++ src/main/webapp/resources/modules/signup/sign-up-step-2-form.tsx
@@ -3,17 +3,17 @@ import { Button, Form } from "react-bootstrap"
 export default ({ error, signUpCode, submitFormCallback }) => {
   return (
     <Form className="standard-form" id="sign-up-step-2-form" onSubmit={submitFormCallback}>
-      <h3 className="mb-3 fw-normal">Sign Up</h3>
+      <h3 className="mb-3 fw-normal">确认注册</h3>
       {
         error
           ? <p className="error-message">{error}</p>
           : null
       }
-      <Form.FloatingLabel label="Username" className="mb-1">
-        <Form.Control type="text" name="username" placeholder="Username" required autoFocus />
+      <Form.FloatingLabel label="用户名" className="mb-1">
+        <Form.Control type="text" name="username" placeholder="用户名" required autoFocus />
       </Form.FloatingLabel>
       <input type="hidden" name="signUpCode" value={signUpCode} />
-      <Button type="submit" variant="primary" className="mt-3 w-100 btn-lg">Confirm Sign Up</Button>
+      <Button type="submit" variant="primary" className="mt-3 w-100 btn-lg">确认注册</Button>
     </Form>
   )
 }
