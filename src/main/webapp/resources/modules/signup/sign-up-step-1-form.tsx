3b0bd97991fdb6b4bc39c2962f20fd86feaa27ca
fisher101 <134185919+fisher101@users.noreply.github.com>
2025-01-26 11:58:22 +0800

    Update sign-up-step-1-form.tsx

--- src/main/webapp/resources/modules/signup/sign-up-step-1-form.tsx
+++ src/main/webapp/resources/modules/signup/sign-up-step-1-form.tsx
@@ -3,18 +3,18 @@ import { Button, Form } from "react-bootstrap"
 export default ({ error, submitFormCallback }) => {
   return (
     <Form className="standard-form" id="sign-up-step-1-form" onSubmit={submitFormCallback}>
-      <h3 className="mb-3 fw-normal">Sign Up</h3>
+      <h3 className="mb-3 fw-normal">注册账号</h3>
       { error ? <p className="error-message">{error}</p> : null }
-      <Form.FloatingLabel label="Username" className="mb-1">
-        <Form.Control type="text" name="username" placeholder="Username" required autoFocus />
+      <Form.FloatingLabel label="设置用户名" className="mb-1">
+        <Form.Control type="text" name="username" placeholder="设置用户名" required autoFocus />
       </Form.FloatingLabel>
-      <Form.FloatingLabel label="Password" className="mb-1">
-        <Form.Control type="password" name="password" placeholder="Password" required />
+      <Form.FloatingLabel label="设置密码" className="mb-1">
+        <Form.Control type="password" name="password" placeholder="设置密码" required />
       </Form.FloatingLabel>
-      <Form.FloatingLabel label="Email" className="mb-1">
-        <Form.Control type="email" name="emailAddress" placeholder="Email" required />
+      <Form.FloatingLabel label="设置Email（随意编）" className="mb-1">
+        <Form.Control type="email" name="emailAddress" placeholder="设置Email（随意编）" required />
       </Form.FloatingLabel>
-      <Button type="submit" variant="primary" className="mt-3 w-100 btn-lg">Sign Up</Button>
+      <Button type="submit" variant="primary" className="mt-3 w-100 btn-lg">确认注册</Button>
     </Form>
   )
 }
