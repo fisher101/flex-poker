fb6795f7b815ff5010589a2d22d73cda5b5f87c1
fisher101 <134185919+fisher101@users.noreply.github.com>
2025-01-26 11:59:42 +0800

    Update sign-up-step-1-success.tsx

--- src/main/webapp/resources/modules/signup/sign-up-step-1-success.tsx
+++ src/main/webapp/resources/modules/signup/sign-up-step-1-success.tsx
@@ -1,9 +1,9 @@
 export default ({ username, email }) => {
   return (
     <>
-      <h3 className="mb-3 fw-normal">Confirm your sign up</h3>
+      <h3 className="mb-3 fw-normal">确认注册</h3>
       <p>Email sent to {email} (TODO: not working yet)</p>
-      <p><a href={`/sign-up-confirm?username=${username}`}>Click here to confirm</a></p>
+      <p><a href={`/sign-up-confirm?username=${username}`}>点击这里确认</a></p>
     </>
   )
 }
